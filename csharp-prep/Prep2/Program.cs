831b6899cba65fc206c170cf1f582fc1c8b2ed4f
Evan <evan@evanalexander.dev>
2024-10-26 17:37:58 -0600

    Week 1: Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+        string sign = "";
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+
+        if ((letter == "A" && sign == "+") || letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Grade: {letter}{sign}");
+        Console.WriteLine(grade >= 70 ? "You passed!" : "Better luck next time!");
     }
 }

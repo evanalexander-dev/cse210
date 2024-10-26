08410598e673d4662daa6653b7c39df7ef64dd7c
Evan <evan@evanalexander.dev>
2024-10-26 17:49:24 -0600

    Week 1: Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        string play = "yes";
+        while (play == "yes")
+        {
+            int randomNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int guessCounter = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCounter++;
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCounter} guesses.");
+                }
+            } while (guess != randomNumber);
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            play = Console.ReadLine();
+        }
     }
 }

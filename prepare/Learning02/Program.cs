b8835ba2224e3fe09ba0ff4a1e2ad0c0e7dfbec4
Evan <evan@evanalexander.dev>
2024-10-28 12:27:21 -0600

    Week 2: Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Developer";
+        job2._company = "Google";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }

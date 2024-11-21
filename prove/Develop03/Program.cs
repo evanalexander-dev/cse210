c7e15f5c3808dac1ec8bc8344ca0411aa9d8821b
Evan <evan@evanalexander.dev>
2024-11-21 15:31:06 -0700

    W03: Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,25 @@
-using System;
+// I made the program only hide words that aren't already hidden. (Scripture.cs, HideRandomWords method)
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        var scripture = new Scripture(new Reference("1 Nephi", 3, 7), "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+
+        string choice;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            choice = Console.ReadLine();
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        } while (choice != "quit");
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,49 @@
+﻿public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        foreach (var word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        var random = new Random();
+        var i = 0;
+        do
+        {
+            if (IsCompletelyHidden())
+            {
+                break;
+            }
+
+            var word = _words[random.Next(_words.Count)];
+            if (word.IsHidden())
+            {
+                continue;
+            }
+
+            word.Hide();
+            i++;
+        } while (i < numberToHide);
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> words = [];
+        words.AddRange(_words.Select(word => word.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {string.Join(" ", words)}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(word => word.IsHidden());
+    }
+}
